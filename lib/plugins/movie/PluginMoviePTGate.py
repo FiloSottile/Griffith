28c3844d8ef3bf8acb69f8f15adfdecd9fc9b437
Vasco Nunes <vasco.m.nunes@gmail.com>
2007-01-10 23:56:42 +0000

    PTGate movie plugin updated

--- lib/plugins/movie/PluginMoviePTGate.py
+++ lib/plugins/movie/PluginMoviePTGate.py
@@ -32,7 +32,7 @@ plugin_url = "www.cinema.ptgate.pt"
 plugin_language = _("Portuguese")
 plugin_author = "Vasco Nunes"
 plugin_author_email="<vasco.m.nunes@gmail.com>"
-plugin_version = "0.2"
+plugin_version = "0.3"
 
 class Plugin(movie.Movie):
 	def __init__(self, id):
@@ -92,12 +92,19 @@ class Plugin(movie.Movie):
 		self.country = gutils.trim(self.page,"s</b><br>","<br><br><b>g")
 
 	def get_rating(self):
-		self.rating = ""
+		tmp=gutils.trim(self.page,"img src='Images/ustar",".gif")
+		if(tmp):
+			self.rating = float(tmp)
+			if (self.rating>10):
+				self.rating = int(self.rating/10)
+				self.rating = self.rating*2
+		else:
+			self.rating=""
 
 class SearchPlugin(movie.SearchMovie):
 	def __init__(self):
-		self.original_url_search	= "http://cinema.ptgate.pt/pesquisa.php?pesquisa="
-		self.translated_url_search	= "http://cinema.ptgate.pt/pesquisa.php?pesquisa="
+		self.original_url_search	= "http://cinema.ptgate.pt/pesquisa-filme.php?titulo="
+		self.translated_url_search	= "http://cinema.ptgate.pt/pesquisa-filme.php?titulo="
 		self.encode='iso-8859-1'
 
 	def search(self,parent_window):
@@ -106,10 +113,10 @@ class SearchPlugin(movie.SearchMovie):
 		return self.page
 
 	def sub_search(self):
-		self.page = gutils.trim(self.page,"class=group>pesquisa por filme</b>", "class=group>pesquisa por int")
+		self.page = gutils.trim(self.page,"<!-- DISPLAY RESULTS -->", "<!-- /DISPLAY RESULTS -->")
 
 	def get_searches(self):
-		elements = string.split(self.page,"<br></td>")
+		elements = string.split(self.page,"</td><td>")
 		self.number_results = elements[-1]
 
 		if (len(elements[0])):
