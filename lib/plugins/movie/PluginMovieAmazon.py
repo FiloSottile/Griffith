76dcc6f96b2cbc16c4682eed0e12887e8d79d2c1
Michael Jahn <mikej06@hotmail.com>
2008-10-26 21:28:32 +0000

    [#179618] During "Get from Web ", griffith window keeps stealing focus

--- lib/plugins/movie/PluginMovieAmazon.py
+++ lib/plugins/movie/PluginMovieAmazon.py
@@ -52,7 +52,6 @@ class Plugin(movie.Movie):
 		# dont use base functionality
 		# use the Amazon Web API
 		self.parent_window = parent_window
-		progress = movie.Progress(parent_window,_('Searching'),_('Wait a moment'))
 		try:
 			locale = self.config.get('amazon_locale', 0, section='add')
 			if locale == '1':
@@ -67,20 +66,20 @@ class Plugin(movie.Movie):
 				locale = 'jp'
 			else:
 				locale = None
-			retriever = AmazonRetriever(self.movie_id, locale, parent_window, progress, self.debug, 'Get')
+			retriever = AmazonRetriever(self.movie_id, locale, parent_window, self.progress, self.debug, 'Get')
 			retriever.start()
 			while retriever.isAlive():
-				progress.pulse()
+				self.progress.pulse()
 				while gtk.events_pending():
 					gtk.main_iteration()
 			self.page = retriever.result.Item
 		except:
+			self.page = ''
 			try:
 				self.debug.show("Error retrieving results from amazon.")
 				self.debug.show(retriever.result.Request.Errors.Error.Message)
 			except:
 				pass
-		progress.close()
 		return self.page
 
 	def get_image(self):
@@ -228,7 +227,6 @@ class SearchPlugin(movie.SearchMovie):
 		# use the Amazon Web API
 		self.titles = [""]
 		self.ids = [""]
-		progress = movie.Progress(parent_window,_('Searching'),_('Wait a moment'))
 		try:
 			locale = self.config.get('amazon_locale', 0, section='add')
 			if locale == '1':
@@ -243,10 +241,10 @@ class SearchPlugin(movie.SearchMovie):
 				locale = 'jp'
 			else:
 				locale = None
-			retriever = AmazonRetriever(self.title.encode('iso8859-1'), locale, parent_window, progress, self.debug)
+			retriever = AmazonRetriever(self.title.encode('iso8859-1'), locale, parent_window, self.progress, self.debug)
 			retriever.start()
 			while retriever.isAlive():
-				progress.pulse()
+				self.progress.pulse()
 				while gtk.events_pending():
 					gtk.main_iteration()
 			self.page = retriever.result
@@ -256,7 +254,6 @@ class SearchPlugin(movie.SearchMovie):
 				self.debug.show(retriever.result.Request.Errors.Error.Message)
 			except:
 				pass
-		progress.close()
 		return self.page
 
 	def get_searches(self):
