70bf76b016749af2b0849e7c966d974b1922d9b3
Michael Jahn <mikej06@hotmail.com>
2009-01-28 21:31:03 +0000

    FilmAffinity movie plugin updated

--- lib/plugins/movie/PluginMovieFilmAffinity.py
+++ lib/plugins/movie/PluginMovieFilmAffinity.py
@@ -23,41 +23,43 @@ __revision__ = '$Id: PluginMovieFilmAffinity.py 389 2006-07-29 18:43:35Z piotrek
 
 import gutils
 import movie
-import string
+import string, re
 
-plugin_name        = 'FilmAffinity'
-plugin_description    = 'Base de Datos de Peliculas'
-plugin_url        = 'www.filmaffinity.com'
-plugin_language        = _('Spanish')
-plugin_author        = 'Pedro D. Sánchez'
-plugin_author_email    = '<pedrodav@gmail.com>'
-plugin_version        = '0.2'
+plugin_name         = 'FilmAffinity'
+plugin_description  = 'Base de Datos de Peliculas'
+plugin_url          = 'www.filmaffinity.com'
+plugin_language     = _('Spanish')
+plugin_author       = 'Pedro D. Sánchez'
+plugin_author_email = '<pedrodav@gmail.com>'
+plugin_version      = '0.3'
 
 class Plugin(movie.Movie):
     def __init__(self, id):
-        self.encode='iso-8859-15'
+        self.encode   = 'iso-8859-15'
         self.movie_id = id
-        self.url = "http://www.filmaffinity.com/es/film%s.html" % str(self.movie_id)
+        self.url      = "http://www.filmaffinity.com/es/film%s.html" % str(self.movie_id)
 
     def get_image(self):
-        tmp = string.find(self.page, '<img src="http://www.filmaffinity.com/imgs/movies/')
+        tmp = string.find(self.page, 'www.filmaffinity.com/imgs/movies/')
         if tmp == -1:
             self.image_url = ''
         else:
-            self.image_url = gutils.trim(self.page[tmp-1:], 'src="', '"')
+            self.image_url = 'http://' + gutils.before(self.page[tmp:], '"')
 
     def get_o_title(self):
         self.o_title = gutils.trim(self.page, '<b>TITULO ORIGINAL</b></td>', '</b></td>')
         self.o_title = gutils.after(self.o_title, '<b>')
+        self.o_title = re.sub('[ ]+', ' ', self.o_title)
 
     def get_title(self):
-        self.title = gutils.trim(self.page, '<img src="http://www.filmaffinity.com/images/movie.gif" border="0"> ', '</span>')
+        self.title = gutils.trim(self.page, 'www.filmaffinity.com/images/movie.gif" border="0"> ', '</span>')
+        self.title = re.sub('[ ]+', ' ', self.title)
 
     def get_director(self):
         self.director = gutils.trim(self.page,'<b>DIRECTOR</b></td>', '</td>')
 
     def get_plot(self):
-        self.plot = gutils.trim(self.page, '<b>GÉNERO Y CRÍTICA</b>', '<br />')
+        self.plot = gutils.trim(self.page, u'<b>GÉNERO Y CRÍTICA</b>', '<br />')
         if self.plot == '':
             self.plot = gutils.trim(self.page, '<b>G&Eacute;NERO Y CR&Iacute;TICA</b>', '<br />')
         self.plot = gutils.after(self.plot, '<td valign="top">')
@@ -70,17 +72,17 @@ class Plugin(movie.Movie):
         self.plot = string.replace(self.plot, '(FILMAFFINITY)', '')
 
     def get_year(self):
-        self.year = gutils.trim(self.page, '<b>AÑO</b></td>', '</td>')
-        self.year = gutils.after(self.year, '<td >')
+        self.year = gutils.trim(self.page, u'<b>AÑO</b></td>', '</td>')
+        self.year = gutils.clean(self.year)
 
     def get_runtime(self):
-        self.runtime = gutils.trim(self.page, '<b>DURACIÓN</b></td>', ' min.</td>')
+        self.runtime = gutils.trim(self.page, u'<b>DURACIÓN</b></td>', ' min.</td>')
         if self.runtime == '':
             self.runtime = gutils.trim(self.page, '<b>DURACI&Oacute;N</b></td>', ' min.</td>')
         self.runtime = gutils.after(self.runtime[-10:], '<td>')
 
     def get_genre(self):
-        self.genre = gutils.trim(self.page, '<b>GÉNERO Y CRÍTICA</b>', '<br />')
+        self.genre = gutils.trim(self.page, u'<b>GÉNERO Y CRÍTICA</b>', '<br />')
         if self.genre == '':
             self.genre = gutils.trim(self.page, '<b>G&Eacute;NERO Y CR&Iacute;TICA</b>', '<br />')
         self.genre = gutils.trim(self.genre, '<td valign="top">', '/')
@@ -89,9 +91,9 @@ class Plugin(movie.Movie):
     def get_cast(self):
         self.cast = ''
         self.cast = gutils.trim(self.page, '<b>REPARTO</b></td>', '</td>')
-        self.cast = string.replace(self.cast, '</a>,', '\n')
+        self.cast = re.sub('</a>,[ ]*', '\n', self.cast)
         self.cast = string.strip(gutils.strip_tags(self.cast))
-        self.cast = string.replace(self.cast, '  ', '')
+        self.cast = re.sub('[ ]+', ' ', self.cast)
 
     def get_classification(self):
         self.classification = ''
@@ -111,20 +113,29 @@ class Plugin(movie.Movie):
         self.trailer = ''
 
     def get_country(self):
-        self.country = gutils.trim(self.page, '<b>PAÍS</b></td>', '</td>')
-        self.country = gutils.trim(self.country, 'alt="', '"')
+        self.country = gutils.trim(self.page, u'<b>PAÍS</b></td>', '</td>')
+        tmp = gutils.trim(self.country, 'alt="', '"')
+        if tmp == '':
+            self.country = gutils.trim(self.country, 'title="', '"')
+        else:
+            self.country = tmp
 
     def get_rating(self):
         self.rating = gutils.trim(self.page, '<tr><td align="center" style="color:#990000; font-size:22px; font-weight: bold;">', '</td></tr>')
         if self.rating:
-            self.rating = str(float(gutils.clean(string.replace(self.rating, ',', '.'))))
+            self.rating = str(round(float(gutils.clean(string.replace(self.rating, ',', '.')))))
+
+    def get_cameraman(self):
+        self.cameraman = gutils.trim(self.page, u'<b>FOTOGRAFÍA</b></td>', '</td>')
+        if self.cameraman == '':
+            self.cameraman = gutils.trim(self.page, '<b>FOTOGRAF&Iacute;A</b></td>', '</td>')
 
 class SearchPlugin(movie.SearchMovie):
 
     def __init__(self):
-        self.original_url_search    = 'http://www.filmaffinity.com/es/search.php?stype=title&stext='
-        self.translated_url_search    = 'http://www.filmaffinity.com/es/search.php?stype=title&stext='
-        self.encode = 'iso-8859-15'
+        self.original_url_search   = 'http://www.filmaffinity.com/es/search.php?stype=title&stext='
+        self.translated_url_search = 'http://www.filmaffinity.com/es/search.php?stype=title&stext='
+        self.encode                = 'iso-8859-15'
 
     def search(self,parent_window):
         if not self.open_search(parent_window):
@@ -133,12 +144,12 @@ class SearchPlugin(movie.SearchMovie):
         self.sub_search()
         if self.page <> '':
             return self.page
-        auxPage = gutils.trim(auxPage, '<b>TU CRÍTICA</b></div>', '</a>')
+        auxPage = gutils.trim(auxPage, u'<b>TU CRÍTICA</b></div>', '</a>')
         self.page = gutils.trim(auxPage, 'movie_id=', '">')
         return self.page
 
     def sub_search(self):
-        self.page = gutils.trim(self.page, 'Resultados por título</span>', '<br>')
+        self.page = gutils.trim(self.page, u'Resultados por título</span>', '<br>')
         #self.page = gutils.after(self.page, '</td></tr><tr><td><b>')
         #self.page = self.page.decode('iso-8859-15')
 
@@ -152,6 +163,6 @@ class SearchPlugin(movie.SearchMovie):
 
             if (elements[0]<>''):
                 for element in elements[:-1]:
-                    self.ids.append(gutils.trim(element, '<b><a href="/es/film','.html'))
+                    self.ids.append(gutils.trim(element, '<b><a href="/es/film', '.html'))
                     title = gutils.after(element, '<b><a href="/es/film')
                     self.titles.append(gutils.strip_tags(gutils.convert_entities(gutils.after(title, '>'))))
