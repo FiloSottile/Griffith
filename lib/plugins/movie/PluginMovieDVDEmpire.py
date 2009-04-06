a4ad1ff5b6f3a5969e8267bfd540e4c97f7535c9
Michael Jahn <mikej06@hotmail.com>
2009-04-06 14:44:05 +0000

    [#348226] DVD Empire lookup problem

--- lib/plugins/movie/PluginMovieDVDEmpire.py
+++ lib/plugins/movie/PluginMovieDVDEmpire.py
@@ -2,7 +2,7 @@
 
 __revision__ = '$Id$'
 
-# Copyright (c) 2007
+# Copyright (c) 2007-2009
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -25,20 +25,20 @@ import gutils
 import movie
 import re
 
-plugin_name = "DVD Empire"
-plugin_description = "International Retailer of DVD Movies"
-plugin_url = "www.dvdempire.com"
-plugin_language = _("English")
-plugin_author = "Michael Jahn"
+plugin_name         = "DVD Empire"
+plugin_description  = "International Retailer of DVD Movies"
+plugin_url          = "www.dvdempire.com"
+plugin_language     = _("English")
+plugin_author       = "Michael Jahn"
 plugin_author_email = "<mikej06@hotmail.com>"
-plugin_version = "1.1"
+plugin_version      = "1.1"
 
 class Plugin(movie.Movie):
     def __init__(self, id):
-        self.encode='iso-8859-1'
+        self.encode   = 'iso-8859-1'
         self.movie_id = id
-        self.url = "http://www.dvdempire.com/Exec/v4_item.asp?item_id=" + str(self.movie_id)
-    
+        self.url      = "http://www.dvdempire.com/Exec/v4_item.asp?item_id=" + str(self.movie_id)
+
     def get_image(self):
         tmp_page = gutils.trim(self.page, '<td align="center" valign="top" class="fontsmall">', '</td>')
         self.image_url = gutils.trim(tmp_page, '<img src=\'', '\' ')
@@ -56,7 +56,8 @@ class Plugin(movie.Movie):
 
     def get_plot(self):
         self.plot = gutils.strip_tags(gutils.trim(self.page, '<td width="100%" valign="top" class="fontsmall3">', '</td>'))
-        self.plot = self.plot.replace('\x93', '"')
+        self.plot = self.plot.replace(u'\x93', '"')
+        self.plot = self.plot.replace(u'\x84', '"')
 
     def get_year(self):
         self.year = gutils.strip_tags(gutils.trim(self.page, '>Production Year:', '<br />'))
@@ -110,29 +111,41 @@ class Plugin(movie.Movie):
         tmp_page = tmp_page.replace('<br />', '\n')
         tmp_page = gutils.strip_tags(tmp_page)
         if tmp_page <> '':
+            tmp_page = re.sub('[\r\n]+', '\n', tmp_page)
             self.notes = self.notes + '\nFeatures:' + tmp_page + '\n'
         tmp_page = gutils.trim(self.page, 'Video:', '<b>')
         tmp_page = tmp_page.replace('<br>', '\n')
         tmp_page = tmp_page.replace('<br />', '\n')
         tmp_page = gutils.strip_tags(tmp_page)
         if tmp_page <> '':
+            tmp_page = re.sub('[\r\n]+', '\n', tmp_page)
             self.notes = self.notes + '\nVideo:' + tmp_page + '\n'
         tmp_page = gutils.trim(self.page, 'Audio:', '<b>')
         tmp_page = tmp_page.replace('<br>', '\n')
         tmp_page = tmp_page.replace('<br />', '\n')
         tmp_page = gutils.strip_tags(tmp_page)
         if tmp_page <> '':
+            tmp_page = re.sub('[\r\n]+', '\n', tmp_page)
             self.notes = self.notes + '\nAudio:' + tmp_page
         tmp_page = gutils.trim(self.page, 'Subtitles:', '<b>')
         tmp_page = gutils.strip_tags(tmp_page)
         if tmp_page <> '':
+            tmp_page = re.sub('[\r\n]+', '\n', tmp_page)
             self.notes = self.notes + '\nSubtitles:\n' + tmp_page + '\n'
+
+    def get_screenplay(self):
+        self.screenplay = gutils.strip_tags(gutils.trim(self.page, '>Writers:', '</a>'))
+        self.screenplay = self.screenplay.replace('&nbsp;', '')
+        self.screenplay = self.screenplay.replace('&#149;', '')
+
+    def get_barcode(self):
+        self.barcode = gutils.trim(self.page, '<b>UPC Code:</b>', '<')
 
 class SearchPlugin(movie.SearchMovie):
     def __init__(self):
         self.original_url_search   = "http://www.dvdempire.com/Exec/v1_search_all.asp?&site_media_id=0&pp=&search_refined=32&used=0&string="
         self.translated_url_search = "http://www.dvdempire.com/Exec/v1_search_all.asp?&site_media_id=0&pp=&search_refined=32&used=0&string="
-        self.encode = 'iso-8859-1'
+        self.encode                = 'iso-8859-1'
 
     def search(self,parent_window):
         if not self.open_search(parent_window):
@@ -165,16 +178,81 @@ class SearchPlugin(movie.SearchMovie):
         return self.page
 
     def get_searches(self):
-        split_pattern = re.compile('<a[\t ]+href=["\']/Exec/v4_item[.]asp[?]userid=[-0-9]+[&]amp;item_id=')
-        check_pattern = re.compile('[0-9]+[&]amp;searchID=[0-9]+["\']>')
+        split_pattern = re.compile('<a[\t ]+href=["\']/Exec/v4_item[.]asp[?](?:userid=[-0-9]+[&]amp;)*item_id=([0-9]+)\'')
         elements = split_pattern.split(self.page)
-        elements[0] = ''
-        for element in elements:
-            if element <> '':
-                is_an_item = check_pattern.search(element)
-                if is_an_item:
-                    tmp_title = gutils.strip_tags(gutils.trim(element, ">", "</a>"))
-                    if tmp_title <> '':
-                        tmp_id = gutils.before(element,'&')
-                        self.ids.append(tmp_id)
-                        self.titles.append(tmp_title)
+        for index in range(1, len(elements), 2):
+            title_element = elements[index + 1]
+            tmp_title = gutils.strip_tags(gutils.trim(title_element, '>', '</a>'))
+            if tmp_title <> '':
+                self.ids.append(elements[index])
+                type = re.search('<b>Blu-ray</b>', title_element)
+                if type:
+                    tmp_title = tmp_title + ' (Blu-ray)'
+                type = re.search('<b>DVD-Video</b>', title_element)
+                if type:
+                    tmp_title = tmp_title + ' (DVD)'
+                self.titles.append(tmp_title)
+
+#
+# Plugin Test
+#
+class SearchPluginTest(SearchPlugin):
+    #
+    # Configuration for automated tests:
+    # dict { movie_id -> [ expected result count for original url, expected result count for translated url ] }
+    #
+    test_configuration = {
+        'Rocky Balboa' : [ 2, 2 ],
+    }
+
+class PluginTest:
+    #
+    # Configuration for automated tests:
+    # dict { movie_id -> dict { arribute -> value } }
+    #
+    # value: * True/False if attribute only should be tested for any value
+    #        * or the expected value
+    #
+    test_configuration = {
+        '1300280' : { 
+            'title'               : 'Rocky Balboa',
+            'o_title'             : 'Rocky Balboa',
+            'director'            : 'Sylvester Stallone',
+            'plot'                : True,
+            'cast'                : 'Sylvester Stallone\n\
+Burt Young\n\
+Geraldine Hughes\n\
+Milo Ventimiglia\n\
+Antonio Tarver',
+            'country'             : False,
+            'genre'               : 'Sports, Boxing, Drama',
+            'classification'      : 'PG',
+            'studio'              : 'Sony Pictures Home Entertainment',
+            'o_site'              : False,
+            'site'                : 'http://www.dvdempire.com/Exec/v4_item.asp?item_id=1300280',
+            'trailer'             : False,
+            'year'                : 2006,
+            'notes'               : 'Features:\n\
+Deleted Scenes & Alternate Ending\n\
+Boxing\'s Bloopers\n\
+Skill Vs. Will: The Making of Rocky Balboa\n\
+Reality in the Ring: Filming Rocky\'s Final Fight\n\
+Virtual Champion: Creating the Computer Fight\n\
+Audio Commentary with Sylvester Stallone\n\
+\n\
+Video:\n\
+Widescreen 1.85:1 Color (Anamorphic) \n\
+\n\
+Audio:\n\
+ENGLISH: Dolby Digital 5.1 [CC]\n\
+FRENCH: Dolby Digital 5.1\n\
+\n\
+Subtitles:\n\
+English, Spanish, French',
+            'runtime'             : 102,
+            'image'               : True,
+            'rating'              : 9,
+            'screenplay'          : 'Sylvester Stallone',
+            'barcode'             : '043396161900'
+        },
+    }
